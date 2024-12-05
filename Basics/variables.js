c84489e96cb3ef51afa31de4bdf17d973d20814d
Vineet Gulia <105794537+vineetgulia00@users.noreply.github.com>
2024-12-05 06:33:43 +0000

    container config done

---
+++ Basics/variables.js
@@ -0,0 +1,17 @@
+const accountid = 12345
+let accountemail = "levithan@gmail.com"
+var accountpassword = "12345"
+accountcity = "delhi"
+let accountstate;
+
+// accountid = 2
+// const value m ek bar jo de diya voh de diya usko change nhi kr sakte
+
+console.log(accountid)
+
+console.table([accountid, accountemail,accountpassword, accountcity, accountstate])
+// used to print all the given values in a table
+
+// always use let because var has scope problem means agr kisi ne var wali value change kri toh voh har jgh change hojaegi. 
+
+
