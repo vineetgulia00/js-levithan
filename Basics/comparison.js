f2c60adca3d9a41f5abcbe808dff3ea672f1fe4a
Vineet Gulia <105794537+vineetgulia00@users.noreply.github.com>
2024-12-05 08:19:03 +0000

    comparison

---
+++ Basics/comparison.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 >! 1);
+
+// console.log("2" == 1);
+// console.log(2 >! "1");
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// === strict check in this it will check datatypes also after that only it will go for checking
+
+console.log("2" === 2)
+
