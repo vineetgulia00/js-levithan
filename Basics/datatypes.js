a9575f76e3545b001033cbf6c10a18bb49da0df0
Vineet Gulia <105794537+vineetgulia00@users.noreply.github.com>
2024-12-05 06:49:22 +0000

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; 
+// treat all js code as a newer version.
+
+let name = "levithan"
+let age = 10
+let isloggedin = false
+
+//nuber = 2 to power 53
+//bigint
+//string
+//boolean = true/false
+//null = standalone value
+//undefined = 
+//symbol = unique
+
+console.log(typeof "levithan");
+console.log(typeof age);
+console.log(typeof null);
+
+
