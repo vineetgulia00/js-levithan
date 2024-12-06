0937e60cdf5312e3651712269b62664532852fe8
Vineet Gulia <105794537+vineetgulia00@users.noreply.github.com>
2024-12-06 08:23:16 +0000

    number and math

---
+++ Basics/num.js
@@ -0,0 +1,34 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+
+// const othernumber = 12.1324
+
+// console.log( othernumber.toPrecision(2))
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-in'))
+
+
+// // -----------MATHS-----------
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// // change neg number to positive
+// console.log(Math.round(4.7))
+// // round off the number 
+
+// // console.log(Math.random())
+// // console.log(Math.random()*10+1)
+// // console.log((Math.random()*10)+1)
+
+const min = 1
+const max = 6
+
+console.log(Math.floor(
+    Math.random() * (max - min + 1)) +min)
+
